d25b8b05b2f6586fb426021287fcb9c76ff7533a
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-09-29 00:11:22 +0200

    Add ContactSection component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import AboutSection from './components/AboutSection/AboutSection';
 import BranchLocator from './components/BranchLocator/BranchLocator';
 import BrandsSection from './components/BrandsSection/BrandsSection';
+import ContactSection from './components/ContactSection/ContactSection';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import HeroSection from './components/HeroSection/HeroSection';
@@ -19,6 +20,7 @@ export default function Home() {
       <SpecialsSection />
       <BrandsSection />
       <BranchLocator />
+      <ContactSection />
       <Footer />
     </div>
   );
