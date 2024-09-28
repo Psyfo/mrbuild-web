ec9b9fa6d6e187bd0275d7a385476d323ba3f716
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-09-29 00:12:24 +0200

    Refactor SpecialsSection component to update styling and add carousel buttons

--- src/app/components/SpecialsSection/SpecialsSection.tsx
+++ src/app/components/SpecialsSection/SpecialsSection.tsx
@@ -19,17 +19,37 @@ const SpecialsSection: React.FC = () => {
       />
 
       {/* Swiper Slider for Specials */}
-      <div className='w-full mt-[2rem] mb-[6rem] px-[2rem]'>
+      <div className='relative w-full my-[2rem] px-[2rem]'>
         <SpecialsSlider />
+        {/* Carousel buttons */}
+        <Image
+          src='/images/services/arrow_left.svg'
+          alt='Left arrow'
+          width={20}
+          height={20}
+          className='hidden lg:block absolute top-1/2 left-12 z-20 transform -translate-y-1/2 custom-prev w-[50px]  h-[50px] lg:h-[90px]'
+          id='services-prev'
+        ></Image>
+        <Image
+          src='/images/services/arrow_right.svg'
+          alt='Right arrow'
+          width={20}
+          height={20}
+          className='hidden lg:block absolute top-1/2 right-12 z-20 transform -translate-y-1/2 custom-next w-[50px] h-[50px] lg:h-[90px]'
+          id='services-next'
+        ></Image>
       </div>
 
       {/* Follow links */}
-      <div className='flex flex-col gap-[1.5rem] items-center'>
+      <div className='flex flex-col gap-[1.5rem] items-center mt-[6rem]'>
         <span className='font-dinot text-white text-center text-[1rem] lg:text-[1.6rem] tracking-[0.04rem] mb-[2rem]'>
           Follow us to stay up to date with our specials
         </span>
         <div className='flex gap-[2rem]'>
-          <a href='https://www.facebook.com/profile.php?id=61551736583171'>
+          <a
+            href='https://www.facebook.com/profile.php?id=61551736583171'
+            target='_blank'
+          >
             <div className='flex items-center gap-[1rem]'>
               <Image
                 src='/images/specials/icon_facebook.svg'
@@ -41,7 +61,7 @@ const SpecialsSection: React.FC = () => {
               <div className='font-dino text-[1rem] text-white'>Mr Build</div>
             </div>
           </a>
-          <a href='https://www.instagram.com/mrbuild_sa/?hl=en'>
+          <a href='https://www.instagram.com/mrbuild_sa/?hl=en' target='_blank'>
             <div className='flex items-center gap-[1rem]'>
               <Image
                 src='/images/specials/icon_instagram.svg'
