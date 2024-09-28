fc420082ec62186a7da43aa0a76b7b6ffb2e707e
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-09-29 00:11:50 +0200

    Refactor section heading and brand marquee padding in BrandsSection component

--- src/app/components/BrandsSection/BrandsSection.tsx
+++ src/app/components/BrandsSection/BrandsSection.tsx
@@ -6,12 +6,12 @@ const BrandsSection: React.FC = () => {
   return (
     <section id='brands' className='bg-white w-full'>
       {/* Section Heading */}
-      <div className='my-[4rem]'>
+      <div className='py-[4rem]'>
         <SectionHeading title='Brands' color='black' />
       </div>
 
       {/* Brand Marquee */}
-      <div className='mb-[4rem]'>
+      <div className='pb-[4rem]'>
         <BrandMarquee />
       </div>
     </section>
