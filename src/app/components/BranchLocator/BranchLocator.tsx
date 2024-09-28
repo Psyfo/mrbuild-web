41d2e484972e3bef310f4dbb2e233dd96a791283
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-09-29 00:11:42 +0200

    Refactor BranchLocator component styles and layout

--- src/app/components/BranchLocator/BranchLocator.tsx
+++ src/app/components/BranchLocator/BranchLocator.tsx
@@ -5,31 +5,31 @@ import SectionHeading from '@/components/SectionHeading/SectionHeading';
 
 const BranchLocator: React.FC = () => {
   return (
-    <section className='bg-mbDark'>
+    <section id='branch-locator' className='bg-mbDark'>
       {/* Section Heading */}
       <div className='flex flex-col items-center py-[4rem]'>
         <SectionHeading
           title='Branch Locator'
           subtitle='Find your nearest Mr. Build branch easily with our branch locator. We have conveniently located stores across South Africa. Locate your nearest branch today and let us assist you with all your construction needs.'
         />
-        <div className='round border-[3px] border-mbYellow rounded-lg mt-[1rem] px-[4rem] py-[1rem] font-dinot text-[1rem] lg:text-[1.9rem] text-white tracking-[0.04rem]'>
+        <div className='round border-[3px] border-mbYellow rounded-lg mt-[1rem] mx-[0] px-[1rem] lg:px-[4rem] py-[1rem] font-dinot text-[1rem] lg:text-[1.9rem] text-white tracking-[0.04rem]'>
           <div className='mb-[1rem] lg:mb-[2rem]'>Our operating hours are:</div>
           <div className='flex'>
-            <span className='w-[8rem] lg:w-[15rem]'>Mon - Fri</span>
-            <span>: 07:30 - 13:00, 14:00 - 16:30</span>
+            <span className='w-[6rem] lg:w-[15rem]'>Mon - Fri</span>
+            <span>07:30 - 13:00, 14:00 - 16:30</span>
           </div>
           <div className='flex'>
-            <span className='w-[8rem] lg:w-[15rem]'>Sat</span>
-            <span>: 07:30 - 12:30</span>
+            <span className='w-[6rem] lg:w-[15rem]'>Sat</span>
+            <span>07:30 - 12:30</span>
           </div>
 
           <div className='flex'>
-            <span className='w-[8rem] lg:w-[15rem]'>Sun</span>
-            <span>: Closed</span>
+            <span className='w-[6rem] lg:w-[15rem]'>Sun</span>
+            <span>Closed</span>
           </div>
           <div className='flex'>
-            <span className='w-[8rem] lg:w-[15rem]'>Public Holidays</span>
-            <span>: Hours might differ</span>
+            <span className='w-[6rem] lg:w-[15rem]'>Public Holidays</span>
+            <span>Hours might differ</span>
           </div>
         </div>
       </div>
