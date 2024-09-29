642602ac987b7d0853239dd5b44f862ae2cbbbc0
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-09-29 12:42:04 +0200

    Refactor IconBar component to use SVG icons instead of images

--- src/app/components/HeroSection/IconBar/IconBar.tsx
+++ src/app/components/HeroSection/IconBar/IconBar.tsx
@@ -1,33 +1,40 @@
-import React from "react";
+import Image from 'next/image';
+import React from 'react';
 
 // Replace with actual icons
 const IconBar = () => {
   // Array of image sources and names for your icons
   const iconData = [
-    { src: '/images/header/icon_electrical.png', name: 'Electrical' },
-    { src: '/images/header/icon_plumbing.png', name: 'Plumbing' },
-    { src: '/images/header/icon_tools.png', name: 'Tools' },
+    { src: '/images/header/icon_electrical.svg', name: 'Electrical' },
+    { src: '/images/header/icon_plumbing.svg', name: 'Plumbing' },
+    { src: '/images/header/icon_tools.svg', name: 'Tools' },
     {
-      src: '/images/header/icon_building-material.png',
+      src: '/images/header/icon_building-material.svg',
       name: 'Building Material',
     },
-    { src: '/images/header/icon_paint.png', name: 'Paint' },
-    { src: '/images/header/icon_doors.png', name: 'Wrapped Doors' },
-    { src: '/images/header/icon_cutting.png', name: 'Edging & Cutting' },
-    { src: '/images/header/icon_diy.png', name: 'DIY' },
+    { src: '/images/header/icon_paint.svg', name: 'Paint' },
+    { src: '/images/header/icon_doors.svg', name: 'Wrapped Doors' },
+    { src: '/images/header/icon_cutting.svg', name: 'Edging & Cutting' },
+    { src: '/images/header/icon_diy.svg', name: 'DIY' },
   ];
 
   return (
-    <div
-      className='box-border grid grid-cols-4 lg:grid-cols-8 gap-y-4 items-center bg-mbRed w-[95vw] px-[3.125rem] py-[1rem] absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 transform z-10 rounded-2xl lg:rounded-full'
-      style={{ gridAutoRows: 'minmax(0, auto)' }}
-    >
+    <div className='flex flex-wrap gap-y-4 items-start justify-center bg-mbRed w-[95vw] md:w-[65vw] lg:w-[65%] h-auto px-[1rem] lg:px-[3.125rem] py-[1rem] absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 transform z-10 rounded-2xl xl:rounded-full lg:border-r lg:border-white'>
       {iconData.map(({ src, name }, idx) => (
-        <div key={idx} className='flex flex-col items-center min-h-[4rem]'>
-          <div className='flex items-center justify-center w-[2rem] lg:w-[3.2rem] h-[2rem] lg:h-[3.2rem]'>
-            <img src={src} alt={name} className='h-full' />
+        <div
+          key={idx}
+          className='flex flex-col items-center justify-center w-1/4 xl:flex-1 h-auto lg:h-auto py-[1rem]'
+        >
+          <div className='flex items-center justify-center w-auto h-[3rem] lg:h-[4rem]'>
+            <Image
+              src={src}
+              alt={name}
+              width={50}
+              height={50}
+              className='w-auto h-full'
+            />
           </div>
-          <span className='text-white text-center text-[0.6rem] lg:text-[0.8rem] mt-[10px] tracking-[0.08rem] flex justify-center'>
+          <span className='text-white text-center text-[0.8rem] lg:text-[1rem] mt-[10px] tracking-[0.1rem]'>
             {name}
           </span>
         </div>
