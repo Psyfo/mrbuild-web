58b358aaef1c30d97dd89769857830e7e04ac011
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-09-29 15:57:33 +0200

    Refactor HeroSection component to add fade-in animation to hero image

--- src/app/components/HeroSection/HeroSection.tsx
+++ src/app/components/HeroSection/HeroSection.tsx
@@ -1,20 +1,34 @@
+'use client';
 import IconBar from './IconBar/IconBar';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 export default function HeroSection() {
+  // Define the animation variants for the hero image
+  const imageVariants = {
+    hidden: { opacity: 0, y: 50 }, // Start below and transparent
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } }, // End at normal position
+  };
+
   return (
     <section
       id='hero'
       className='w-full h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center relative bg-[url("/images/header/mb_hero_bg.png")]'
     >
-      {/* Hero Text Image */}
-      <Image
-        src='/images/header/logo_mb_red.svg'
-        alt='Hero Text'
-        width={800}
-        height={600}
+      {/* Hero Text Image with motion for animation */}
+      <motion.div
+        variants={imageVariants}
+        initial='hidden'
+        animate='visible' // Always animate to visible on load
         className='max-w-full max-h-full object-contain w-[80%] md:w-[60%] lg:w-[50%]'
-      ></Image>
+      >
+        <Image
+          src='/images/header/logo_mb_red.svg'
+          alt='Hero Text'
+          width={800}
+          height={600}
+        />
+      </motion.div>
 
       <IconBar />
     </section>
