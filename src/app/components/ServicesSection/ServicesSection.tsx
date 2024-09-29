24d6b44c743cc091ed142f7c16b09b2f60ea1970
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-09-29 16:16:03 +0200

    Refactor ServicesSection component to add fade-in animation to section heading and services slider

--- src/app/components/ServicesSection/ServicesSection.tsx
+++ src/app/components/ServicesSection/ServicesSection.tsx
@@ -1,27 +1,83 @@
 'use client';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import SectionHeading from '@/components/SectionHeading/SectionHeading';
 import ServicesSlider from './ServicesSlider/ServicesSlider';
+import { motion } from 'framer-motion';
 
-// import ServicesSlider from './ServicesSlider/ServicesSlider';
+/* eslint-disable react-hooks/exhaustive-deps */
 
 const ServicesSection: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect(); // Stop observing once the section is visible
+        }
+      },
+      { threshold: 0.1 } // Adjust this value as needed
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
+  // Variants for the fade-in animations
+  const headingVariants = {
+    hidden: { opacity: 0, y: -20 }, // Start above
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1 },
+    },
+  };
+
+  const sliderVariants = {
+    hidden: { opacity: 0, y: 20 }, // Start below
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1, delay: 0.5 }, // Delay to match the heading animation
+    },
+  };
+
   return (
     <section
+      ref={sectionRef}
       id='services'
       className='relative w-full bg-mbDark border-t-[9px] border-mbYellow'
     >
       {/* Section Heading */}
-      <div className='my-[2rem]'>
+      <motion.div
+        variants={headingVariants}
+        initial='hidden'
+        animate={isVisible ? 'visible' : 'hidden'}
+        className='my-[2rem]'
+      >
         <SectionHeading
           title='Our Services'
           subtitle='Tailored services for every space and vision'
         />
-      </div>
+      </motion.div>
 
       {/* Swiper Slider for Services */}
-      <div className='w-full mt-[2rem] px-[2rem] pb-[4rem]'>
+      <motion.div
+        variants={sliderVariants}
+        initial='hidden'
+        animate={isVisible ? 'visible' : 'hidden'}
+        className='w-full mt-[2rem] px-[2rem] pb-[4rem]'
+      >
         <ServicesSlider />
         <div className='lg:absolute top-[2rem] right-[2rem] flex mt-[2rem] gap-[2rem]'>
           <Image
@@ -31,7 +87,7 @@ const ServicesSection: React.FC = () => {
             height={20}
             className='custom-prev w-[50px] lg:w-[90px] h-[50px] lg:h-[90px]'
             id='services-prev'
-          ></Image>
+          />
           <Image
             src='/images/services/arrow_right.svg'
             alt='Right arrow'
@@ -39,9 +95,9 @@ const ServicesSection: React.FC = () => {
             height={20}
             className='custom-next w-[50px] lg:w-[90px] h-[50px] lg:h-[90px]'
             id='services-next'
-          ></Image>
+          />
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
