601ab07ecf881df19c11c6bc51fd2852d7d740e0
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-09-29 15:27:23 +0200

    Refactor HeroSection component to update logo and image styling

--- src/app/components/HeroSection/HeroSection.tsx
+++ src/app/components/HeroSection/HeroSection.tsx
@@ -1,16 +1,19 @@
-import IconBar from "./IconBar/IconBar";
-import Image from "next/image";
+import IconBar from './IconBar/IconBar';
+import Image from 'next/image';
 
 export default function HeroSection() {
   return (
-    <section id="hero" className='w-full h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center relative bg-[url("/images/header/mb_hero_bg.png")]'>
+    <section
+      id='hero'
+      className='w-full h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center relative bg-[url("/images/header/mb_hero_bg.png")]'
+    >
       {/* Hero Text Image */}
       <Image
-        src='/images/header/mb_hero.png'
+        src='/images/header/logo_mb_red.svg'
         alt='Hero Text'
         width={800}
         height={600}
-        className='max-w-full max-h-full object-contain w-[50rem]'
+        className='max-w-full max-h-full object-contain w-[80%] md:w-[60%] lg:w-[50%]'
       ></Image>
 
       <IconBar />
