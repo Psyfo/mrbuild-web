b336e5edc91bc5c2fefa04e1bc2a0a776cecb752
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-09-29 16:27:14 +0200

    Refactor SpecialsSection component to add fade-in animation to section heading, specials slider, and follow links

--- src/app/components/SpecialsSection/SpecialsSection.tsx
+++ src/app/components/SpecialsSection/SpecialsSection.tsx
@@ -1,25 +1,93 @@
 'use client';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import SectionHeading from '@/components/SectionHeading/SectionHeading';
 import SpecialsSlider from './SpecialsSlider/SpecialsSlider';
+import { motion } from 'framer-motion';
+
+/* eslint-disable react-hooks/exhaustive-deps */
 
 const SpecialsSection: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect(); // Stop observing once the section is visible
+        }
+      },
+      { threshold: 0.1 } // Adjust this value as needed
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
+  // Variants for animations
+  const headingVariants = {
+    hidden: { opacity: 0, y: -20 }, // Start above
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1 },
+    },
+  };
+
+  const sliderVariants = {
+    hidden: { opacity: 0, y: 20 }, // Start below
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1, delay: 0.5 }, // Fade in after heading
+    },
+  };
+
+  const followLinksVariants = {
+    hidden: { opacity: 0, y: 20 }, // Start below
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1, delay: 1 }, // Fade in after slider
+    },
+  };
+
   return (
     <section
+      ref={sectionRef}
       id='specials'
       className='flex flex-col w-full py-[4rem] bg-mbDark border-t-[9px] border-mbYellow'
     >
       {/* Section Heading */}
-      <SectionHeading
-        title='Exclusive In-Store Specials for the Month'
-        subtitle='Discover unbeatable deals on top-quality tools, hardware, and building
+      <motion.div
+        variants={headingVariants}
+        initial='hidden'
+        animate={isVisible ? 'visible' : 'hidden'}
+      >
+        <SectionHeading
+          title='Exclusive In-Store Specials for the Month'
+          subtitle='Discover unbeatable deals on top-quality tools, hardware, and building
           supplies. Visit us in-store this month for limited-time offers you
           won’t want to miss!'
-      />
+        />
+      </motion.div>
 
       {/* Swiper Slider for Specials */}
-      <div className='relative w-full my-[2rem] px-[2rem]'>
+      <motion.div
+        variants={sliderVariants}
+        initial='hidden'
+        animate={isVisible ? 'visible' : 'hidden'}
+        className='relative w-full my-[2rem] px-[2rem]'
+      >
         <SpecialsSlider />
         {/* Carousel buttons */}
         <Image
@@ -27,9 +95,9 @@ const SpecialsSection: React.FC = () => {
           alt='Left arrow'
           width={20}
           height={20}
-          className='hidden lg:block absolute top-1/2 left-12 z-20 transform -translate-y-1/2 custom-prev w-[50px]  h-[50px] lg:h-[90px]'
+          className='hidden lg:block absolute top-1/2 left-12 z-20 transform -translate-y-1/2 custom-prev w-[50px] h-[50px] lg:h-[90px]'
           id='services-prev'
-        ></Image>
+        />
         <Image
           src='/images/services/arrow_right.svg'
           alt='Right arrow'
@@ -37,11 +105,16 @@ const SpecialsSection: React.FC = () => {
           height={20}
           className='hidden lg:block absolute top-1/2 right-12 z-20 transform -translate-y-1/2 custom-next w-[50px] h-[50px] lg:h-[90px]'
           id='services-next'
-        ></Image>
-      </div>
+        />
+      </motion.div>
 
       {/* Follow links */}
-      <div className='flex flex-col gap-[1.5rem] items-center mt-[6rem]'>
+      <motion.div
+        variants={followLinksVariants}
+        initial='hidden'
+        animate={isVisible ? 'visible' : 'hidden'}
+        className='flex flex-col gap-[1.5rem] items-center mt-[6rem]'
+      >
         <span className='font-dinot text-white text-center text-[1rem] lg:text-[1.6rem] tracking-[0.04rem] mb-[2rem]'>
           Follow us to stay up to date with our specials
         </span>
@@ -49,6 +122,7 @@ const SpecialsSection: React.FC = () => {
           <a
             href='https://www.facebook.com/profile.php?id=61551736583171'
             target='_blank'
+            rel='noopener noreferrer'
           >
             <div className='flex items-center gap-[1rem]'>
               <Image
@@ -61,7 +135,11 @@ const SpecialsSection: React.FC = () => {
               <div className='font-dino text-[1rem] text-white'>Mr Build</div>
             </div>
           </a>
-          <a href='https://www.instagram.com/mrbuild_sa/?hl=en' target='_blank'>
+          <a
+            href='https://www.instagram.com/mrbuild_sa/?hl=en'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
             <div className='flex items-center gap-[1rem]'>
               <Image
                 src='/images/specials/icon_instagram.svg'
@@ -74,7 +152,7 @@ const SpecialsSection: React.FC = () => {
             </div>
           </a>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
