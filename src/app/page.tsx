b5c0ce5d48e22d003c0db0d52422fe9a884afdd1
Sipho Mahlangu <21422869@dut4life.ac.za>
2024-11-18 23:29:00 +0200

    Removed Announcements section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
-import AboutSection from "./components/AboutSection/AboutSection";
-import Announcement from "./components/Announcement/Announcement";
-import BranchLocator from "./components/BranchLocator/BranchLocator";
-import BrandsSection from "./components/BrandsSection/BrandsSection";
-import ContactSection from "./components/ContactSection/ContactSection";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import HeroSection from "./components/HeroSection/HeroSection";
-import NumbersSection from "./components/NumbersSection/NumbersSection";
-import ServicesSection from "./components/ServicesSection/ServicesSection";
-import SpecialsSection from "./components/SpecialsSection/SpecialsSection";
+import AboutSection from './components/AboutSection/AboutSection';
+import Announcement from './components/Announcement/Announcement';
+import BranchLocator from './components/BranchLocator/BranchLocator';
+import BrandsSection from './components/BrandsSection/BrandsSection';
+import ContactSection from './components/ContactSection/ContactSection';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import HeroSection from './components/HeroSection/HeroSection';
+import NumbersSection from './components/NumbersSection/NumbersSection';
+import ServicesSection from './components/ServicesSection/ServicesSection';
+import SpecialsSection from './components/SpecialsSection/SpecialsSection';
 
 export default function Home() {
   return (
     <div>
-      <Announcement />
       <Header />
       <HeroSection />
       <AboutSection />
